a6b4da50c71aecc44060be6b8a688a4b69006532
bincysoja2021 <bincysoja2017@gmail.com>
2024-04-11 14:40:06 +0530

    uodations

--- app/Http/Controllers/Searchcontoller.php
+++ app/Http/Controllers/Searchcontoller.php
@@ -24,27 +24,6 @@ class Searchcontoller extends Controller
     {
       return view('admin.advanced_search');
     }
-    public function normal_search(Request $req)
-    {
-      if ($req->ajax())
-      {
-          $user_name_exist=Document::where('user_name',$req->search_key)->exists();
-          $data = Document::where('deleted_at',NULL)->latest()->get();
-          return Datatables::of($data)
-              ->addIndexColumn()
-              ->addColumn('action', function($row)
-              {
-                $actionBtn = '
-                              <a href="'.route('download.pdf', $row->filename).'"><i class="fa fa-download" aria-hidden="true"></i></a>';
-                return $actionBtn;
-              })
-              ->addColumn('thumbnail', function ($row) {
-              $actionBtn ="<img src='".route('load_images', $row->thumbnail)."'  width='100px' height='100px' >";
-              return $actionBtn;
-              })
-              ->rawColumns(['thumbnail','action'])
-              ->make(true);
-       }    
-      
-    }
+
+    
 }
--- resources/views/admin/include/search.blade.php
+++ resources/views/admin/include/search.blade.php
@@ -1,15 +1,22 @@
-<form action="{{ url('normal_search') }}" method="POST">
-  <div class="search-box">
-      <div class="input-group row">
-        <div class="col-md-9">
-          <input type="text" placeholder="" class="form-control">
-          <label>(Search using Invoice numbers, Sales order numbers, shipping bill numbers, client name, ect.)</label>
-        </div>
-        <div class="col-md-3">
-          <input type="submit" placeholder="" class="btn btn-primary" value="Search">
-          <label class="search-label"><a href="{{url('/advanced_search')}}">Advanced Search</a></label>
-        </div>
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+<div class="search-box">
+    <div class="input-group row">
+      <div class="col-md-9">
+        <input type="text" placeholder="" class="form-control">
+        <label>(Search using Invoice numbers, Sales order numbers, shipping bill numbers, client name, ect.)</label>
       </div>
-  </div>
+      <div class="col-md-3">
+        <input type="submit" placeholder="" class="btn btn-primary" value="Search" name="Search" id="Search">
+        <label class="search-label"><a href="{{url('/advanced_search')}}">Advanced Search</a></label>
+      </div>
+    </div>
+</div>
+</body>
 
-</form>
+</html>
+
